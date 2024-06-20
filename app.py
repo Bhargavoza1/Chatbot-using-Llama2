bcf5546fda9030a033852e5467dc7758c9f093c4
Bhargavoza1 <66210089+Bhargavoza1@users.noreply.github.com>
2024-06-20 08:40:41 +0530

    update

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 from flask import Flask, render_template, jsonify, request
 from langchain_pinecone import PineconeVectorStore
-
 from src.helper import download_hugging_face_embeddings
-from langchain.vectorstores import Pinecone
-import pinecone
 from langchain.prompts import PromptTemplate
 from langchain.llms import CTransformers
 from langchain.chains import RetrievalQA
--- store_index.py
+++ store_index.py
@@ -1,6 +1,4 @@
 from src.helper import load_pdf, text_split, download_hugging_face_embeddings
-from langchain.vectorstores import Pinecone
-import pinecone
 from dotenv import load_dotenv
 import os
 from langchain_pinecone import PineconeVectorStore
